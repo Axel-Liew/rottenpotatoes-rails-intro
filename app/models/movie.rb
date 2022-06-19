5c686ecac72829a84400d65b49cbf5b77c7a3997
Axel-Liew <107032895+Axel-Liew@users.noreply.github.com>
2022-06-19 23:02:20 +0800

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,12 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    return %w[G PG PG-13 R]
+  end
+
+  def self.with_ratings(ratings_list)
+    if ratings_list.nil? || ratings_list.empty?
+      return self.all
+    end
+    return self.where(rating: ratings_list)
+  end
 end
